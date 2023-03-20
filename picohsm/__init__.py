54d3264cd579c4234aba4cae8c32d7a901ecbfe6
Pol Henarejos <pol.henarejos@cttc.es>
2023-03-20 17:56:20 +0100

    Add EncryptionMode flag to root.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- picohsm/__init__.py
+++ picohsm/__init__.py
@@ -1,6 +1,5 @@
 from ._version import __version__
-from .PicoHSM import PicoHSM
-from .PicoHSM import KeyType
+from .PicoHSM import PicoHSM, KeyType, EncryptionMode
 from .DO import DOPrefixes
 from .Algorithm import Algorithm, Padding
 from .APDU import APDUResponse
